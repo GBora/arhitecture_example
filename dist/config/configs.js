6e49d9834f4ee13e4e91c1ce2f7e0a2e8c592d86
George Bora <gpbora@gmail.com>
2020-01-07 19:37:10 +0200

    Need to build

--- dist/config/configs.js
+++ dist/config/configs.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    dbURL: 'database.db'
+    dbURL: 'database.db',
+    port: 8080,
+    host: '0.0.0.0'
 };
 // let storageLocation = 'C:\\Users\\george\\Documents\\GitHub\\architecture_example\\db\\database.db';
 //# sourceMappingURL=configs.js.map
--- dist/main.js
+++ dist/main.js
@@ -7,11 +7,13 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const routesIndex_1 = __importDefault(require("./routes/routesIndex"));
+const configs_1 = __importDefault(require("./config/configs"));
 const app = express_1.default();
 app.use(body_parser_1.default.json());
 app.use(cors_1.default());
 app.use("/", routesIndex_1.default);
-app.listen(3000, () => {
-    console.log('server online');
+let port = process.env.PORT || configs_1.default.port;
+app.listen(port, () => {
+    console.log('server online on port', port);
 });
 //# sourceMappingURL=main.js.map
