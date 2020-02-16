e10eda50c7d698ebaeeb4d561f15fbe0f5b8cc9e
George Bora <gbora@pitechnologies.ro>
2020-02-16 12:36:55 +0200

    build

--- dist/main.js
+++ dist/main.js
@@ -6,13 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const routesIndex_1 = __importDefault(require("./routes/routesIndex"));
 const configs_1 = __importDefault(require("./config/configs"));
 const app = express_1.default();
 app.use(body_parser_1.default.json());
 app.use(cors_1.default());
-app.use("/", routesIndex_1.default);
-app.use(express_1.default.static("./public/"));
+// The application will have it's routes on /api
+app.use("/api", routesIndex_1.default);
+app.use(express_1.default.static(path_1.default.join(__dirname + '/public/')));
+// Everithing else will be redirected to the FE app
+app.all("/*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname + '/public/index.html'));
+});
 let port = process.env.PORT || configs_1.default.port;
 app.listen(port, () => {
     console.log('server online on port', port);
--- dist/routes/messages/messagesRoutes.js
+++ dist/routes/messages/messagesRoutes.js
@@ -5,10 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const messagesController_1 = __importDefault(require("../../controllers/messagesController"));
+const pubsub_js_1 = __importDefault(require("pubsub-js"));
 const messagesRoutes = express_1.Router();
 const messageCtrl = new messagesController_1.default();
 messagesRoutes.post("/add-message", (req, res) => {
     messageCtrl.addMessage(req.body);
+    console.log('PUB');
+    pubsub_js_1.default.publish('NEW MESSAGE', 'hello world!');
     res.json({ success: true });
 });
 messagesRoutes.post("/get-conversation", (req, res) => {
@@ -20,6 +23,17 @@ messagesRoutes.post("/get-conversation", (req, res) => {
     catch (_a) {
         res.sendStatus(500);
     }
+});
+messagesRoutes.get("/conversation-stream", (req, res) => {
+    // res.status(200).set({
+    //     "Content-Type": "text/event-stream",
+    //     "Connection": "keep-alive",
+    //     "Cache-Control": "no-cache"
+    // })
+    // var token = PubSub.subscribe('NEW MESSAGE', () => {
+    //     console.log('SUB')
+    //     res.write('bla bla');
+    // });
 });
 exports.default = messagesRoutes;
 //# sourceMappingURL=messagesRoutes.js.map
