4975207b2dcf3cfe2cc0cc6fc93e4e2d0be7f6e0
George Bora <gbora@pitechnologies.ro>
2020-03-27 23:56:29 +0200

    new build

--- dist/public/polyfills-es2015.js
+++ dist/public/polyfills-es2015.js
@@ -6370,8 +6370,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\george\Documents\GitHub\architecture-front\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\george\Documents\GitHub\architecture-front\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /home/gbora/Documents/Code/architecture/arhitecture-front/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/gbora/Documents/Code/architecture/arhitecture-front/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
