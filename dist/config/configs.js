bcc86ea4b05ad98f6e4f4f880592fd3790651f96
George Bora <gbora@pitechnologies.ro>
2020-04-11 21:33:57 +0300

    build

--- dist/config/configs.js
+++ dist/config/configs.js
@@ -3,7 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     dbURL: 'database.db',
     port: 8080,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    dbName: 'guarded_eyrie',
+    dbUser: 'root',
+    dbPassword: '',
+    dbHost: 'localhost',
+    dbDialect: 'mysql'
 };
 // let storageLocation = 'C:\\Users\\george\\Documents\\GitHub\\architecture_example\\db\\database.db';
 //# sourceMappingURL=configs.js.map
