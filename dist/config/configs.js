c7490325906d0ee24b0d869f11fc50c9b5671e01
George Bora <gbora@pitechnologies.ro>
2020-04-12 19:57:09 +0300

    fixed db seetings

--- dist/config/configs.js
+++ dist/config/configs.js
@@ -1,19 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// TODO I know it's not ok to store my credentials like this, it's a test app give me a break
 exports.default = {
     // dbURL: 'database.db',
     port: 8080,
     host: '0.0.0.0',
-    dbName: 'guarded_eyrie',
-    dbUser: 'root',
-    dbPassword: '',
-    dbHost: 'localhost',
+    // dbName: 'guarded_eyrie',
+    // dbUser: 'root',
+    // dbPassword: '',
+    // dbHost: 'localhost',
+    // dbDialect: 'mysql'    
+    dbName: 'heroku_8f2d4f86b0e3d37',
+    dbUser: 'bf006de9882f55',
+    dbPassword: 'b2f61d39',
+    dbHost: 'us-cdbr-iron-east-01.cleardb.net',
     dbDialect: 'mysql'
-    // dbName: 'heroku_8f2d4f86b0e3d37',
-    // dbUser: 'bf006de9882f55',
-    // dbPassword: 'b2f61d39',
-    // dbHost: 'us-cdbr-iron-east-01.cleardb.net',
-    // dbDialect: 'mysql'
 };
-// let storageLocation = 'C:\\Users\\george\\Documents\\GitHub\\architecture_example\\db\\database.db';
 //# sourceMappingURL=configs.js.map
--- src/config/configs.ts
+++ src/config/configs.ts
@@ -1,19 +1,18 @@
+// TODO I know it's not ok to store my credentials like this, it's a test app give me a break
 export default {
     // dbURL: 'database.db',
     port: 8080,
     host: '0.0.0.0',
 
-    dbName: 'guarded_eyrie',
-    dbUser: 'root',
-    dbPassword: '',
-    dbHost: 'localhost',
-    dbDialect: 'mysql'    
+    // dbName: 'guarded_eyrie',
+    // dbUser: 'root',
+    // dbPassword: '',
+    // dbHost: 'localhost',
+    // dbDialect: 'mysql'    
 
-    // dbName: 'heroku_8f2d4f86b0e3d37',
-    // dbUser: 'bf006de9882f55',
-    // dbPassword: 'b2f61d39',
-    // dbHost: 'us-cdbr-iron-east-01.cleardb.net',
-    // dbDialect: 'mysql'
+    dbName: 'heroku_8f2d4f86b0e3d37',
+    dbUser: 'bf006de9882f55',
+    dbPassword: 'b2f61d39',
+    dbHost: 'us-cdbr-iron-east-01.cleardb.net',
+    dbDialect: 'mysql'
 }
-
-// let storageLocation = 'C:\\Users\\george\\Documents\\GitHub\\architecture_example\\db\\database.db';
